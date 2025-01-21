59d5c58e42011fd863b7160b3c8fe1e8a934c84f
gernotfasching <gernotfasch@gmail.com>
2025-01-21 13:27:39 +0100

    hilfe

--- pawship/src/components/Footer.js
+++ pawship/src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
       <div className="container">
         <div className="social-media">
           <h3>Social Media</h3>
-          <a href="#">Facebook</a>
-          <a href="#">Instagram</a>
-          <a href="#">Twitter</a>
+          <a>Facebook</a>
+          <a>Instagram</a>
+          <a>Twitter</a>
         </div>
         <div className="copyright">
           <p>&copy; 2025 Tierheim Pawship</p>
